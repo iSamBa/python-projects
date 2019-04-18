3ed35203f88c6834611c141fa5ffd4f0965e6f98
iSamBa <49128005+isamba@users.noreply.github.com>
2019-04-18 14:05:35 +0200

    Code refactoring

--- Milestone-Project1/project/TicTocToe.py
+++ Milestone-Project1/project/TicTocToe.py
@@ -1,93 +1,94 @@
-
-def instructions(lst):
-    num=0  # type: int
-    for i in range (1,4) :
-        for j in range (1,4):
-            num+=1
-            lst.append(num)
+def instructions(main_list):
+    num = 0  # type: int
+    for i in range(1, 4):
+        for j in range(1, 4):
+            num += 1
+            main_list.append(num)
     print('\nIn this game we will define the positions as shown below :')
 
-def clear(lst):
+
+def clear(main_list):
     for i in range(0, 9):
-        lst[i]='-'
+        main_list[i] = '-'
 
-def update(lst, position, c) :
-    lst[position-1]=c.upper()
 
-def check(myList, c):
+def update(main_list, position, c):
+    main_list[position - 1] = c.upper()
+
+
+def check(my_list, c):
     # Check in lines
 
-    for i in range(0,7,3) :
-        if myList[i] == myList[i + 1] == myList[i+2]==c.upper():
+    for i in range(0, 7, 3):
+        if my_list[i] == my_list[i + 1] == my_list[i + 2] == c.upper():
             return True
 
-    #chenck in columns
-    for j in range(0,3):
-        if myList[j]==myList[j+3]==myList[j+6]==c.upper() :
+    # check in columns
+    for j in range(0, 3):
+        if my_list[j] == my_list[j + 3] == my_list[j + 6] == c.upper():
             return True
 
-    #Check first diagonal
-    if myList[0]==myList[4]==myList[8]==c.upper() :
+    # Check first diagonal
+    if my_list[0] == my_list[4] == my_list[8] == c.upper():
         return True
 
-    #check second diagonal
-    if myList[2]==myList[4]==myList[6]==c.upper() :
+    # check second diagonal
+    if my_list[2] == my_list[4] == my_list[6] == c.upper():
         return True
 
     return False
 
 
-
-def show(lst) :
+def show(main_list):
     print('\n')
-    print(lst[:3])
-    print(lst[3:6])
-    print(lst[6:9])
+    print(main_list[:3])
+    print(main_list[3:6])
+    print(main_list[6:9])
 
 
-lst=[]
-instructions(lst)
-show(lst)
+game = []
+instructions(game)
+show(game)
 
-#Clear the matrix
-clear(lst)
+# Clear the matrix
+clear(game)
 
-#Input from player 1
+# Input from player 1
 print('\n Let\'s start with players choice, please enter the position coordinates.')
 
-possibilities = list(range(1,10))
+possibilities = list(range(1, 10))
 
 result = False
 while result == False and len(possibilities) > 0:
-        position_player1=int(input('\nPlayer 1 : '))
+    position_player1 = int(input('\nPlayer 1 : '))
+    loop = position_player1 in possibilities
+    while not loop:
+        print('Position already taken')
+        position_player1 = int(input('\nPlayer 1 : '))
         loop = position_player1 in possibilities
-        while (loop == False):
-            print('Position already taken')
-            position_player1 = int(input('\nPlayer 1 : '))
-            loop = position_player1 in possibilities
-        else :
-            possibilities.pop(possibilities.index(position_player1))
-            update(lst, position_player1, 'x')
-            show(lst)
-            result = check(lst, 'x')
-            if result:
-                print('\n\tCongratulations Player 1 ! You have just Won the game !')
-                break
+    else:
+        possibilities.pop(possibilities.index(position_player1))
+        update(game, position_player1, 'x')
+        show(game)
+        result = check(game, 'x')
+        if result:
+            print('\n\tCongratulations Player 1 ! You have just Won the game !')
+            break
 
-        position_player2=int(input('\nPlayer 2 : '))
+    position_player2 = int(input('\nPlayer 2 : '))
+    loop = position_player2 in possibilities
+    while not loop:
+        print('Position already taken')
+        position_player2 = int(input('\nPlayer 2 : '))
         loop = position_player2 in possibilities
-        while (loop == False):
-            print('Position already taken')
-            position_player2 = int(input('\nPlayer 2 : '))
-            loop = position_player2 in possibilities
-        else:
-            possibilities.pop(possibilities.index(position_player2))
-            update(lst, position_player2, '0')
-            show(lst)
-            result = check(lst, 'x')
-            if result:
-                print('\n\tCongratulations Player 2 ! You have just Won the game !')
-                break
+    else:
+        possibilities.pop(possibilities.index(position_player2))
+        update(game, position_player2, '0')
+        show(game)
+        result = check(game, 'x')
+        if result:
+            print('\n\tCongratulations Player 2 ! You have just Won the game !')
+            break
 
 if len(possibilities) == 0 and result == False:
     print("\nNO Winner for this game try next time !")
